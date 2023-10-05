92e3be72d0bf5f7196c024e06c8833e4453b4070
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-05 17:16:18 -0400

    normalize css added

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    res.render('homepage', { 
+        projects, 
+        logged_in: req.session.logged_in 
+      });
+}
+);
+
+module.exports = router;
+
+
