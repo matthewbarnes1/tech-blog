aa750add8e92ad6fcf54e7edb721a74e23b2044c
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-15 16:35:19 -0400

    heroku debug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./Controllers');
+const routes = require('./penguins');
 // const helpers = require('./utils');
 
 
